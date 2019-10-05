ec36ff376701cc6fb49cf164c1b8711fb07dfd72
Caleb Owens <caleb.ja.owens@gmail.com>
2019-10-05 11:45:13 -0400

    Added font-awesome and started nav

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>The index of the website!</title>
+	<title>CupWorm Jewlry</title>
 	<!-- BOOTSTRAP -->
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<!-- JQUERY -->
@@ -11,12 +11,27 @@
 	<!-- STYLESHEET -->
 	<link rel="stylesheet" href="style/style.css">
 	<!-- FONT AWESOME -->
-	<link rel="stylesheet" href="style/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="font-awesome/css/all.css">
 	<!-- ESSENTIAL META TAGS -->
 	<meta name="viewport" content="width=device-width, intitial-scale=1, shrink-to-fit-no">
 	<meta charset="utf-8">
 </head>
 <body>
-
+	<!-- NAVIGATION -->
+	<nav>
+		<div class="col-md-12">
+			<ul class="list-inline">
+				<li class="list-inline-item">
+					<i class="fas fa-home"></i>
+				</li>
+				<li class="list-inline-item">
+					<span style="color:var(--red)">Cup</span>Worm
+				</li>
+				<li class="list-inline-item">
+					<p>Browse</p>
+				</li>
+			</ul>
+		</div>
+	</nav>
 </body>
 </html>
