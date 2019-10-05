6c39ccb747eabc1d6a2b5a538cfc7451f2265c58
BuildTools <unconfigured@null.spigotmc.org>
2019-10-04 20:00:44 -0400

    Partial setup

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 <html>
 <head>
 	<title>The index of the website!</title>
+	<!-- BOOTSTRAP -->
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<!-- JQUERY -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- INTERNAL STYLING -->
+	<style type="text/css"></style>
+	<!-- STYLESHEET -->
+	<link rel="stylesheet" href="style/style.css">
+	<!-- FONT AWESOME -->
+	<link rel="stylesheet" href="style/font-awesome/css/font-awesome.min.css">
+	<!-- ESSENTIAL META TAGS -->
+	<meta name="viewport" content="width=device-width, intitial-scale=1, shrink-to-fit-no">
+	<meta charset="utf-8">
 </head>
 <body>
 
