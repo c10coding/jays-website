d57e1816077cf8ab9ea24ab2239dd798632be80d
Caleb Owens <caleb.ja.owens@gmail.com>
2019-10-08 09:55:31 -0400

    Put in more work on the index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<!-- JQUERY -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- JS -->
+	<script type="text/javascript" src="js/script.js"></script>
 	<!-- INTERNAL STYLING -->
 	<style type="text/css"></style>
 	<!-- STYLESHEET -->
@@ -27,17 +29,152 @@
 				<li class="list-inline-item">
 					<span style="color:var(--blue)">Cup</span><span style="color:var(--yellow)">Worm</span>
 				</li>
-				<li class="list-inline-item">
-					<a href="#" id="browseLink">
-						<p>Browse</p>
-					</a>
-				</li>
+				<div style="float:right;" id="shopAndBrowse">
+					<li class="list-inline-item">
+						<a href="#" id="browseLink">
+							<p>Browse</p>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<i class="fas fa-shopping-cart"></i>
+					</li>
+				</div>
+				
 			</ul>
 		</div>
 	</nav>
-	<!-- SLIDESHOW -->
-	<div class="col-lg-12">
-		
+	<!-- BRACELET -->
+	<div class="row" id="braceletRow">
+		<h1>Bracelets</h1>
+
+		<div class="col-lg-3">
+			<div class="braceletImgWrap">
+				<img src="pics/bracelet.jpg">
+			</div>
+
+			<div class="colorButton">
+				
+			</div>
+
+			<ul class="list-inline colorPicker">
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:blue;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:red;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:orange;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:black;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker"></div>
+				</li>
+			</ul>
+
+			<h2>Resin Octagonal Hoop Earrings</h2>
+			<p>$22.50</p>
+			<button>Add to cart</button>
+		</div>
+
+		<div class="col-lg-3">
+			<div class="braceletImgWrap">
+				<img src="pics/bracelet.jpg">
+			</div>
+
+			<div class="colorButton">
+				
+			</div>
+
+			<ul class="list-inline colorPicker">
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:blue;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:red;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:orange;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:black;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker"></div>
+				</li>
+			</ul>
+
+			<h2>Resin Octagonal Hoop Earrings</h2>
+			<p>$22.50</p>
+			<button>Add to cart</button>
+		</div>
+
+		<div class="col-lg-3">
+			<div class="braceletImgWrap">
+				<img src="pics/bracelet.jpg">
+			</div>
+
+			<div class="colorButton">
+				
+			</div>
+
+			<ul class="list-inline colorPicker">
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:blue;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:red;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:orange;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:black;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker"></div>
+				</li>
+			</ul>
+
+			<h2>Resin Octagonal Hoop Earrings</h2>
+			<p>$22.50</p>
+			<button>Add to cart</button>
+		</div>
+
+		<div class="col-lg-3">
+			<div class="braceletImgWrap">
+				<img src="pics/bracelet.jpg">
+			</div>
+
+			<div class="colorButton">
+				
+			</div>
+
+			<ul class="list-inline colorPicker">
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:blue;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:red;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:orange;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker" style="background-color:black;"></div>
+				</li>
+				<li class="list-inline-item">
+					<div class="inColorPicker"></div>
+				</li>
+			</ul>
+
+			<h2>Resin Octagonal Hoop Earrings</h2>
+			<p>$22.50</p>
+			<button>Add to cart</button>
+		</div>
+
 	</div>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Testing");
+$(document).ready(function(){
+	$(".colorButton").click(function(){
+		var val = $(".colorPicker").css("display");
+		
+		if(val == "none"){
+			$(".colorPicker").fadeIn();
+		}else{
+			$(".colorPicker").fadeOut();
+		}
+	});
+
+	$(".inColorPicker").click(function(){
+		var val = $(this).css("background-color");
+		$(".colorButton").css("background-color",val);
+		$(".colorPicker").fadeOut();
+	});
+});
